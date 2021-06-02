cc2aa8171c2e5b05a9ea6fae8da61d602783fb86
Samuel Hernandez <sam@samhza.com>
2021-06-01 22:31:07 -0400

    add download buttons and icons

--- cheesedex.go
+++ cheesedex.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"archive/tar"
+	"archive/zip"
+	"compress/gzip"
 	_ "embed"
 	"flag"
+	"html"
 	"html/template"
 	"io"
 	"io/fs"
@@ -16,7 +20,7 @@ import (
 	"github.com/dustin/go-humanize"
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
-	"github.com/yuin/goldmark/renderer/html"
+	mdhtml "github.com/yuin/goldmark/renderer/html"
 )
 
 //go:embed index.html
@@ -63,13 +67,24 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if stat.IsDir() {
+		dl := r.URL.Query().Get("dl")
+		if dl == "targz" {
+			archiveTarGZ(p, w)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			return
+		} else if dl == "zip" {
+			err := archiveZIP(p, w)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			return
+		}
 		dir := new(IndexContext)
-		err := dir.Populate(file, p)
+		err := dir.Populate(file, basepath, s.dir)
 		if err != nil {
 			log.Fatalln(err)
-		}
-		if s.dir == p {
-			dir.Root = true
 		}
 		err = indexTmpl.Execute(w, dir)
 		if err != nil {
@@ -80,19 +95,140 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.ServeContent(w, r, stat.Name(), stat.ModTime(), file)
 }
 
+func archiveZIP(root string, wr io.Writer) error {
+	w := zip.NewWriter(wr)
+	fsys := os.DirFS(root)
+	err := fs.WalkDir(fsys, ".", func(fpath string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.Type() != 0 {
+			return nil
+		}
+		f, err := fsys.Open(fpath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		info, err := f.Stat()
+		if err != nil {
+			return err
+		}
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+		header.Name = fpath
+		fw, err := w.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(fw, f)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+	return w.Close()
+}
+
+func archiveTarGZ(root string, wr io.Writer) error {
+	zw := gzip.NewWriter(wr)
+	w := tar.NewWriter(zw)
+	fsys := os.DirFS(root)
+	err := fs.WalkDir(fsys, ".", func(fpath string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.Type() != 0 {
+			return nil
+		}
+		f, err := fsys.Open(fpath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		info, err := f.Stat()
+		if err != nil {
+			return err
+		}
+		header, err := tar.FileInfoHeader(info, "")
+		if err != nil {
+			return err
+		}
+		header.Name = fpath
+		err = w.WriteHeader(header)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(w, f)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return zw.Close()
+}
+
 type IndexContext struct {
 	Name   string
-	Files  []fs.FileInfo
+	Path   string
+	Files  []FileInfo
+	Crumbs []Crumb
 	ReadMe *template.HTML
 	Root   bool
 	Icon   string
 }
 
-func (d *IndexContext) Populate(f *os.File, fpath string) error {
-	var err error
-	d.Files, err = f.Readdir(0)
+type FileInfo struct {
+	fs.FileInfo
+	TargetMode fs.FileMode
+}
+
+type Crumb struct {
+	Link, Text string
+}
+
+func (f FileInfo) IconName() string {
+	switch f.Mode().Type() {
+	case fs.ModeDir:
+		return "folder"
+	case fs.ModeSymlink:
+		if f.TargetMode.IsDir() {
+			return "folder-shortcut"
+		}
+		return "file-shortcut"
+	default:
+		return "file"
+	}
+}
+
+func (f FileInfo) mode() fs.FileMode {
+	if f.Mode().Type() == fs.ModeSymlink {
+		return f.TargetMode
+	}
+	return f.Mode()
+}
+
+func (d *IndexContext) Populate(f *os.File, fpath string, root string) error {
+	files, err := f.Readdir(0)
 	if err != nil {
 		return err
+	}
+	d.Files = make([]FileInfo, len(files))
+	for i, f := range files {
+		d.Files[i] = FileInfo{f, 0}
+		if f.Mode().Type() == fs.ModeSymlink {
+			stat, err := os.Stat(path.Join(root, fpath, f.Name()))
+			if err != nil {
+				continue
+			}
+			d.Files[i].TargetMode = stat.Mode()
+		}
 	}
 	stat, err := f.Stat()
 	if err != nil {
@@ -100,34 +236,60 @@ func (d *IndexContext) Populate(f *os.File, fpath string) error {
 	}
 	d.Name = stat.Name()
 	sort.Slice(d.Files, func(i, j int) bool {
-		return d.Files[i].Name() < d.Files[j].Name()
+		var im, jm fs.FileMode = d.Files[i].mode(), d.Files[j].mode()
+		if im.IsDir() == jm.IsDir() {
+			return d.Files[i].Name() < d.Files[j].Name()
+		}
+		if im.IsDir() {
+			return true
+		}
+		return false
 	})
+
+	d.Path = fpath
+	if fpath == "/" {
+		d.Root = true
+	}
+
+	split := strings.Split(fpath, "/")
+	if split[len(split)-1] == "" {
+		split = split[:len(split)-1]
+	}
+	d.Crumbs = make([]Crumb, len(split))
+	for i := range d.Crumbs {
+		segment := split[i]
+		d.Crumbs[i] = Crumb{strings.Repeat("../", len(d.Crumbs)-i-1), segment}
+	}
+
 	for _, finfo := range d.Files {
-		if strings.ToLower(finfo.Name()) != "readme.md" {
+		switch strings.ToLower(finfo.Name()) {
+		case "readme.txt":
+			p, err := os.ReadFile(path.Join(root, fpath, finfo.Name()))
+			if err != nil {
+				return err
+			}
+			escaped := template.HTML("<pre>" + html.EscapeString(string(p)) + "</pre>")
+			d.ReadMe = &escaped
+		case "readme.md":
+			p, err := os.ReadFile(path.Join(root, fpath, finfo.Name()))
+			if err != nil {
+				return err
+			}
+			sb := new(strings.Builder)
+			md := goldmark.New(
+				goldmark.WithExtensions(extension.GFM),
+				goldmark.WithRendererOptions(mdhtml.WithUnsafe()),
+			)
+			err = md.Convert(p, sb)
+			if err != nil {
+				return err
+			}
+			html := template.HTML(sb.String())
+			d.ReadMe = &html
+		default:
 			continue
 		}
-		p, err := os.ReadFile(path.Join(fpath, finfo.Name()))
-		if err != nil {
-			return err
-		}
-		sb := new(strings.Builder)
-		md := goldmark.New(
-			goldmark.WithExtensions(extension.GFM),
-			goldmark.WithRendererOptions(html.WithUnsafe()),
-		)
-		err = md.Convert(p, sb)
-		if err != nil {
-			return err
-		}
-		html := template.HTML(sb.String())
-		d.ReadMe = &html
 		break
 	}
 	return nil
-}
-
-func iconName(finfo os.FileInfo) (string, error) {
-	switch finfo.Mode().Type() {
-	}
-	return "", error
 }
